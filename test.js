26964026dd34611a10c5a273cea837b68b4b67dc
Giulio <Giulio Giovannini>
2018-06-26 13:10:36 +0200

    Removed useless addMiddleware function

--- test.js
+++ test.js
@@ -23,12 +23,12 @@
     hrbac.addRole('admin');
 
     {
-      let middleware = hrbac.getMiddleware('admin');
+      let middleware = hrbac.generateMiddleware('admin');
 
       await middleware(req, null, (err = null) => console.log(err ? err : 'OK'));
     }
 
-    hrbac.addBool('is group owner', async (req) => {
+    hrbac.addBoolFunc('is group owner', async (req) => {
 
       console.log(req.params.groupId);
       return req.params.groupId == 10
@@ -36,17 +36,17 @@
 
     hrbac.addRole('user');
 
-    //hrbac.addBool('is admin or group owner', hrbac.or('admin', 'is group owner'));
+    //hrbac.addBoolFunc('is admin or group owner', hrbac.or('admin', 'is group owner'));
 
     //hrbac.addMiddleware('is admin or group owner', hrbac.getBool('is admin or group owner'));
 
-    hrbac.addMiddleware(
+    hrbac.addBoolFunc(
       'is admin or group owner',
       hrbac.or('admin', hrbac.and('user', 'is group owner'))
     );
 
     {
-      let middleware = hrbac.getMiddleware('is admin or group owner');
+      let middleware = hrbac.generateMiddleware('is admin or group owner');
 
       await middleware(req, null, (err = null) => console.log(err ? err : 'OK'));
     }
